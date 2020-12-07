debdc41857180b9ef3d2848d35a16efc8e85231a
Adrian Osmond <a.c.osmond@gmail.com>
2020-12-07 21:51:30 +0100

    Make passport image load faster

--- src/GameOver.js
+++ src/GameOver.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const GameOver = ({ score, startGame }) => (
   <div className="min-h-screen flex flex-col items-center justify-center py-12">
     <h1 className="text-6xl text-center font-mono">Game Over</h1>
@@ -8,13 +10,19 @@ const GameOver = ({ score, startGame }) => (
         ${score.toLocaleString(undefined, { maximumFractionDigits: 0 })}
       </span>
     </p>
-    <div>
-      <button
-        onClick={startGame}
-        className="mt-12 bg-blue-700 px-4 py-2 rounded-md"
-      >
-        Play again
-      </button>
+    <button
+      onClick={startGame}
+      className="mt-12 bg-blue-700 px-4 py-2 rounded-md"
+    >
+      Play again
+    </button>
+    <div className="mt-8 flex justify-around">
+      <Link to="/instructions" className="text-blue-300 underline">
+        How to play
+      </Link>
+      <Link to="/about" className="text-blue-300 underline">
+        About
+      </Link>
     </div>
   </div>
 );
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom';
-import passport from './assets/passport.svg';
 
 const Welcome = ({ startGame }) => (
   <div className="max-w-2xl mx-auto py-8 px-4 leading-relaxed text-center">
     <h1 className="text-4xl font-mono">North Pole: Border Control</h1>{' '}
-    <img src={passport} alt="" className="mt-8" />
+    <img
+      src={`${process.env.PUBLIC_URL}/passport.svg`}
+      alt=""
+      className="mt-8"
+    />
     <button
       onClick={startGame}
       className="mt-12 bg-blue-700 px-4 py-2 rounded-md"
@@ -15,7 +18,7 @@ const Welcome = ({ startGame }) => (
       <Link to="/instructions" className="text-blue-300 underline">
         How to play
       </Link>
-      <Link to="/about" className="text-blue-300 underline">
+      <Link to="/about" className="text-blue-300 underline ml-32">
         About
       </Link>
     </div>
